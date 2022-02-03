ebc50fa0f06964bd76f21fff2c1c5b881e46675b
Fabricio <fabriciobaeza@gmail.com>
2022-02-02 20:10:14 -0500

    Kaboom

--- game.js
+++ game.js
@@ -48,11 +48,11 @@ scene("game", ({ level, score }) => {
     [
       "yccccccccw", 
       "a        b", 
-      ")        )", 
+      ")     }  )", 
       "a        b", 
       "a        b", 
       "a    $   b", 
-      ")  }     )", 
+      ")        )", 
       "a        b", 
       "xddddddddz"
     ],
@@ -73,7 +73,7 @@ scene("game", ({ level, score }) => {
     "^": [sprite("top-door"), "next-level"],
     '$': [sprite("stairs"), "next-level"],
     "*": [sprite("slicer"), 'slicer', { dir: -1}, 'dangerous'],
-    "}": [sprite("skeletor"), 'dangerous'],
+    "}": [sprite("skeletor"), 'dangerous', 'skeletor', {dir: -1, timer: 0}],
     ")": [sprite("lanterns"), solid()],
     "(": [sprite("fire-pot"), solid()],
   };
@@ -138,6 +138,17 @@ scene("game", ({ level, score }) => {
     player.dir = vec2(0, 1);
   });
 
+  function spawnKaboom(p) {
+    const obj = add([sprite('kaboom'), pos(p), 'kaboom'])
+    wait(1, () => {
+      destroy(obj)
+    })
+  }
+
+  keyPress('space', () => {
+    spawnKaboom(player.pos.add(player.dir.scale(48)))
+  })
+
   const SLICER_SPEED = 100
 
   action('slicer', (s) => {
@@ -148,10 +159,29 @@ scene("game", ({ level, score }) => {
     s.dir = -s.dir
   })
 
+  const SKELETOR_SPEED = 60
+
+  action('skeletor', (s) => {
+    s.move(0, s.dir * SKELETOR_SPEED)
+    s.timer -= dt()
+    if (s.timer <= 0) {
+      s.dir = - s.dir
+      s.timer = rand(5)
+    }
+  })
+
+  collides('skeletor', 'wall', (s) => {
+    s.dir = -s.dir
+  })
+
   player.overlaps('dangerous', () => {
     go('lose', {score: scoreLabel.value})
   })
 
 });
+
+scene('lose', ({score})=> {
+  add([text(score, 32), origin('center'), pos(width()/2, height()/2)])
+})
 
 start("game", { level: 0, score: 0 });
